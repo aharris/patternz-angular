e472ec72f7acb7a27f857f0919399bce2aa9c7be
Ashton Harris <ashton_harris@epam.com>
2015-01-06 15:57:38 -0500

    Added dynamic text to ngIncludes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,62 @@
 (function () {
-    window.app = angular.module('app', []);
-    var app = window.app;
+    var app = angular.module('app', [
+        'ngRoute'
+    ]);
 
-    app.config(function () {
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            // .when('/', {
+            //     templateUrl: 'template.html',
+            //     controller: 'PatternzCtrl',
+            //     controllerAs: 'pat'
+            // })
+            .when('/buttons', {
+                templateUrl: 'template.html',
+                controller: 'PatternzCtrl',
+                controllerAs: 'pz'
+            })
+            .otherwise({redirectTo: '/'});
+    }]);
 
-    });
-
+    // Main App
     app.controller('AppCtrl', ['$scope', function ($scope) {
 
         $scope.data = {
-            title: 'yo dawg'
+            title: 'Patternz'
         };
 
-        // function text(val) {
-        //     $scope.data.text = val;
-        // }
+    }]);
 
+
+    // Pattern Library
+    app.controller('PatternzCtrl', ['$scope', '$http','$location', 'filterFilter', function ($scope, $http, $location, filterFilter) {
+
+        $scope.tree = 'foo';
+
+        $http.get('data/tree.json').success(function (data) {
+            $scope.tree = data;
+            console.log(data);
+
+            $scope.getCurrentPattern();
+        });
+
+        $scope.getCurrentPattern = function () {
+            var tree = $scope.tree,
+                keys = _.keys(tree);
+
+            // console.log(keys);
+            $scope.currentPatterns = {};
+            for (var i = 0; i < keys.length; i++) {
+                var secondKey = _.keys(tree[keys[i]]);
+
+                if (secondKey[0] === $location.$$path.substr(1)) {
+                    $scope.currentPatterns = _.values( _.values($scope.tree[keys[i]])[0] );
+                    // $scope.currentPattern = currentPatternObj[0];
+                    console.log('currentItem: ' + $scope.currentPatterns);
+                }
+                // console.log('key: ' + secondKey[0]);
+                // console.log('path: ' + $location.$$path.substr(1));
+            }
+        };
     }]);
 })();
--- patternz/js/patternz.js
+++ patternz/js/patternz.js
@@ -1,5 +1,35 @@
 (function () {
     var app = window.app;
+    // debugger;
+    // app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    //     $routeProvider
+    //         .when('patternz/styleguide.html/', {
+    //             templateUrl: '../template.html',
+    //             controller: 'PatternzCtrl',
+    //             controllerAs: 'pat'
+    //         })
+    //         .when('patternz/styleguide.html//buttons', {
+    //             templateUrl: '../template.html',
+    //             controller: 'PatternzCtrl',
+    //             controllerAs: 'pat'
+    //         });
+    // }]);
+
+    var routes = angular.module('routes', []);
+
+    routes.config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .when('patternz/styleguide.html/', {
+                templateUrl: '../template.html',
+                controller: 'PatternzCtrl',
+                controllerAs: 'pat'
+            })
+            .when('patternz/styleguide.html//buttons', {
+                templateUrl: '../template.html',
+                controller: 'PatternzCtrl',
+                controllerAs: 'pat'
+            });
+    }]);
 
     app.controller('PatternzCtrl', ['$scope', '$http', function ($scope, $http) {
 

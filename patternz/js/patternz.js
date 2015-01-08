7c43e765d9b389efc77a288f5ddaf520aa08309e
Ashton Harris <ashton_harris@epam.com>
2015-01-08 15:07:40 -0500

    Dynamically generate code snippets in docs

--- patternz/js/patternz.js
+++ patternz/js/patternz.js
@@ -27,7 +27,14 @@
     // Pattern Library
     pz.controller('PatternzCtrl', ['$scope', '$http','$location', 'filterFilter', '$anchorScroll', function ($scope, $http, $location, filterFilter, $anchorScroll) {
 
-        $scope.tree = 'foo';
+        // Files included here will not have markup in docs
+        $scope.noMarkup = {
+            colors: true,
+            grids: true,
+            typography: true
+        };
+
+        $scope.tree = {};
 
         $http.get('data/tree.json').success(function (data) {
             $scope.tree = data;
@@ -41,17 +48,22 @@
                 keys = _.keys(tree);
 
             $scope.currentPatterns = {};
+            $scope.path = {};
             for (var i = 0; i < keys.length; i++) {
                 var secondKeys = _.keys(tree[keys[i]]);
-
                 for (var j=0; j < secondKeys.length; j++) {
-
                     if (secondKeys[j] === $location.$$path.substr(1)) {
-                        console.log('key: ' + keys[i] + '/' + secondKeys[0]);
-                        console.log('path: ' + $location.$$path.substr(1));
-
+                        $scope.patternGroup = secondKeys[j];
                         $scope.currentPatterns = _.values( _.values($scope.tree[keys[i]])[j] );
-                        console.log('currentItem: ' + $scope.currentPatterns);
+                        var thirdKeys = _.keys(tree[keys[i]][secondKeys[j]]);
+                        for (var k = 0; k < thirdKeys.length; k++) {
+                            var shortPath = thirdKeys[k].replace(/\.[^/.]+$/, ""),
+                                patternTitle = shortPath.replace(/_/, " ");
+
+                            $scope.patternTitle = patternTitle;
+                            $scope.path[shortPath] = 'patterns/' + tree[ keys[i] ][ secondKeys[j] ][thirdKeys[k]];
+                            $scope.createMarkup(k, shortPath);
+                        }
                     }
 
                 }
@@ -69,6 +81,40 @@
             // since $location.hash hasn't changed
             $anchorScroll();
           }
+        };
+
+        //Params:
+        // idx - the index of the current pattern being iterated
+        // shortPath - name of the html file to be included
+        $scope.createMarkup = function(idx, shortPath) {
+            $http.get('../' + $scope.path[shortPath]).success(function (data) {
+                var opts = data.match(/opt.\w*/g) || '',
+                    ngTemplate,
+                    ngRepeat;
+
+                $scope.opts = {};
+                $scope.opts[idx] = '';
+
+                for (var i = 0; i < opts.length; i++) {
+                    var opt = opts[i].split('.').splice(1);
+                    if (i === 0) {
+                        $scope.opts[idx] += opt[0] + ': "string' + i + '"';
+                    } else {
+                        $scope.opts[idx] += ', ' + opt[0] + ': "string' + i + '"';
+                    }
+                }
+
+                //ng-include
+                ngRepeat = opts ? '" ng-repeat="opt in [{' + $scope.opts[idx] +'}]' : '';
+                ngTemplate = '<ng-include src="path.' + shortPath + ngRepeat +'"></ng-include>';
+
+                $scope.ngMarkup = $scope.ngMarkup || {};
+                $scope.ngMarkup[idx] = ngTemplate;
+
+                //HTML File
+                $scope.htmlMarkup = $scope.htmlMarkup || {};
+                $scope.htmlMarkup[idx] = data;
+            });
         };
 
     }]);

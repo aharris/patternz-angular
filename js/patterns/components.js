573dcb2909a2ff9c2f38a9ee15ed2595d5df68ee
Ashton Harris <ahdesignsonline@gmail.com>
2015-02-24 01:01:41 -0500

    Add alert directive
    * Currently empty

--- js/patterns/components.js
+++ js/patterns/components.js
@@ -14,4 +14,15 @@
         };
     });
 
+    components.directive('alert', function(){
+        return{
+            restrict: 'E',
+            scope: true,
+            templateUrl: '../patterns/3_components/alerts/alert.html',
+            link: function(scope, elem, attrs){
+                scope.opt = attrs;
+            }
+        };
+    });
+
 })();
